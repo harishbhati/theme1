368643c31485a6e3b8b6dd88bef77c8e1124a8b8
Harish Bhati <hbhati@ex2india.local>
2019-06-20 15:42:15 +0530

    header and fornt import header icons

--- views/header/header.php
+++ views/header/header.php
@@ -1,23 +1,27 @@
 <header>
     <section class="header-top">
         <div class="container">
-            <div class="header-top-left">
-                <?php include 'sociallink.php';?>
-            </div>
-            <div class="header-top-right">
-                <?php include 'callemail.php';?>
+            <div class="header-left-right-holder flex">
+                <div class="header-top-left">
+                    <?php include 'sociallink.php';?>
+                </div>
+                <div class="header-top-right">
+                    <?php include 'callemail.php';?>
+                </div>
             </div>
         </div>
     </section>
     <section class="header-bottom">
         <div class="container">
-            <div class="header-logo">
-                <?php include 'logo.php';?>
-            </div>
-            <div class="header-nav-holder">
-                <nav class="main-nav">
-                    <?php include 'navigation.php';?>
-                </nav>
+            <div class="logo-nav-holder flex">
+                <div class="header-logo">
+                    <?php include 'logo.php';?>
+                </div>
+                <div class="header-nav-holder">
+                    <nav class="main-nav">
+                        <?php include 'navigation.php';?>
+                    </nav>
+                </div>
             </div>
         </div>
     </section>
